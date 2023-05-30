efc65134d49f2cebb37d614e545cb91dc0bd1681
mbrandse <mbrandse@users.noreply.github.com>
2023-05-30 12:50:18 +0900

    Delete Plugins directory

--- Plugins/RamaMeleeWeapon/Source/RamaMeleePlugin/Private/RamaMeleePluginModule.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright 2015 by Nathan "Rama" Iyer. All Rights Reserved.
-#include "RamaMeleePluginPrivatePCH.h"
-
-DEFINE_LOG_CATEGORY(RamaMeleePlugin)
-
-IMPLEMENT_MODULE(FDefaultGameModuleImpl, RamaMeleePlugin);
--- Plugins/RamaSaveSystem/Source/RamaSaveSystem/Private/RamaSaveSystemPrivatePCH.h
+++
@@ -1,7 +0,0 @@
-// Copyright 2018 by Nathan "Rama" Iyer. All Rights Reserved.
-#pragma once
-
-#include "Engine.h"
-#include "RamaSaveSystemClasses.h"
-
-DECLARE_LOG_CATEGORY_EXTERN(RamaSave, Log, All);
